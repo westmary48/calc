273e9b8ea68d78107a2543f3ecc05d1fb3965e47
Mary West <westmarye1219@gmail.com>
2019-04-09 21:31:26 -0500

    calc works

---
+++ javascripts/helpers/maths.js
@@ -0,0 +1,18 @@
+const add = (num1, num2) => {
+  return num1 * 1 + num2 *1;
+}
+const subtract = (num1, num2) => {
+  return num1 * 1 - num2 *1;
+
+}
+const multiply = (num1, num2) => {
+  return num1 * num2;
+
+}
+const divide = (num1, num2) => {
+  return num1 / num2;
+
+}
+
+
+export default { add, subtract, multiply, divide }
